c41e63175e043577396638b65ccb5e043f3dcb72
kapDelta <maksvasilek2001@gmail.com>
2024-11-26 18:11:14 +0400

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿//
+Console.Write("Введите 1-е число:");
+double chislo1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 2-е число:");
+double chislo2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 3-е число:");
+double chislo3 = Convert.ToDouble(Console.ReadLine());
+double result = chislo1 * chislo2 * chislo3;
+Console.Write($"Результат: {result} "); 
+/* Мне  просто делать нечего -_-
+Console.Write("Введите 1-е число:");
+double chislo1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 2-е число:");
+double chislo2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 3-е число:");
+double chislo3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Выберите действие (введите число): 1. Умножение 2. Деление 3. Сложение 4. Вычитание");
+double choice = Convert.ToDouble(Console.ReadLine());
+if (choice == 1)
+{
+    double result1 = chislo1 * chislo2 * chislo3; 
+    Console.Write($"Результат: {result1}");
+}
+else if (choice == 2)
+{
+    double result2 = chislo1 / chislo2 / chislo3;
+    Console.Write($"Результат: {result2}");
+}
+else if (choice == 3)
+{
+    double result3 = chislo1 + chislo2 + chislo3;
+    Console.Write($"Результат: {result3}");
+}
+else if (choice == 4)
+{
+    double result4 = chislo1 - chislo2 - chislo3;
+    Console.Write($"Результат: {result4}");
+}
+*/
